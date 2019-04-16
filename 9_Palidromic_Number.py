a4201ee9efc016c71c324965ca724caea7711ee4
Yuhang Ji <Kreel@1deMacBook-Pro.local>
2019-04-16 20:34:12 +0800

    100-5.03

--- 9_Palidromic_Number.py
+++ 9_Palidromic_Number.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         t = str(x)
-        if list(t) == list(t)[::-1]:
-            return True
-        return False
+        return t == t[::-1]
 
 if __name__ == '__main__':
-    print(Solution().isPalindrome(-121))    
+    print(Solution().isPalindrome(121))    
