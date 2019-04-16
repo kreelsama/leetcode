ac436a8bf083d7c8b29ab88d79f9e853fd8c4601
Yuhang Ji <Kreel@1deMacBook-Pro.local>
2019-04-16 17:11:05 +0800

    93.27--5.03

---
+++ 9_Palidromic_Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        t = str(x)
+        if list(t) == list(t)[::-1]:
+            return True
+        return False
+
+if __name__ == '__main__':
+    print(Solution().isPalindrome(-121))
