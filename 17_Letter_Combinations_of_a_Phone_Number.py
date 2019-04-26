f1689f62423a7afc89fa710ebe5937dbe6fbbd16
Kreel <stujyh@126.com>
2019-04-26 10:38:18 +0800

    12.7-5.86

---
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,22 @@
+from itertools import product
+
+class Solution:
+    def gen(self, strings):
+        try:
+            for each in strings[0]:
+                for every in self.gen(strings[1:]):
+                    yield (each,) + every
+        except:
+            yield ()
+    
+    def repeat(self, obj, times):
+        for each in range(times):
+            yield obj
+    
+    def letterCombinations(self, digits: str):
+        D = ['', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        mapped = list(map(lambda x, y: y[int(x) - 1], digits,[D] * len(digits)))
+        return list(map(lambda x: ''.join(x), self.gen(mapped)))
+
+if __name__ == '__main__':
+    print(Solution().letterCombinations('23'))
