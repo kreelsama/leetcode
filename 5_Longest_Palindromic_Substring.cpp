869ee1f945c043eebf0f1925d963d2610109cb7a
YHJ <stujyh@126.com>
2019-07-23 14:27:27 +0800

    2019-07-23

--- 5_Longest_Palindromic_Substring.cpp
+++ 5_Longest_Palindromic_Substring.cpp
@@ -1,9 +1,36 @@
 #include <string>
+#include <iostream>
 
 using namespace std;
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        //Manacher's Alogrithm
+    string longestPalindrome_CEA(string s) {//Center expansion alg.
+        string ex("#");
+        string res;
+        for(auto each : s){
+            ex += each;
+            ex += '#';
+        }
+        int longest = 1;
+        int j = 0;
+        for(int i = 0; i < ex.length(); i ++){
+            for(j = 0; j + i < ex.length() && i - j >= 0 && ex[i + j] == ex[i - j]; ++j);
+            --j;
+            int len = j * 2 + 1;
+            if(len > longest){
+                longest = len;
+                res = ex.substr(i - j, len);
+            }
+        }
+        string ret;
+        for(int i = 1; i < res.length(); i += 2){
+            ret += res[i];
+        }
+        return ret;
     }
 };
+
+int main(void){
+    cout<<Solution().longestPalindrome_CEA(string("axbbxaxx"))<<endl;
+    return 0;
+}
--- 7_Reverse_Integer.py
+++ 7_Reverse_Integer.py
@@ -3,9 +3,9 @@ class Solution:
         p = -1 if x < 0 else 1
         x *= p
         res = p*int(''.join(reversed(str(x))))
-        if res <= -pow(2,31) or res >= pow(2,32):
+        if res < -pow(2,31) or res >= pow(2,32) - 1:
             return 0
         return res
 
 if __name__ == "__main__":
-    print(Solution().reverse(1534236469))
+    print(Solution().reverse(1563847412))
