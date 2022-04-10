b4dcd6748e47cf2e4aef6e649e49f39eb072309c
kreelsama <stujyh@126.com>
2022-04-10 23:28:21 +0800

    fix permission

---
+++ 2127.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumInvitations(vector<int>& favorite) {
+        int max_circular_length = 0;
+        
+    }
+};
